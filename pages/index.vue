38956024dee5800622b3ccb5eb2f9349be6fb9f1
Rémy Dumas <remy.dumas@edu.gobelins.fr>
2021-07-05 19:44:23 +0200

    Update description

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <main id="main" v-if="load">
     <div id="content__informations">
       <section class="section__introduction">
-        <p>My name is Rémy Dumas, I'm a young creative developer based in Paris, France. Focusing on details, animations and interactions, I'm studying at the Interactive Design & Development class of Gobelins, l'Ecole de l'image.</p>
+        <p>My name is Rémy Dumas, I'm a young creative developer based in Paris, France. Passionate about digital art and creative technologies, I am currently looking to join an interactive design studio as a creative developer. </p>
       </section>
       <section class="section__projects">
         <h2 class="tt__subtitle fs__sm">Projects</h2>
